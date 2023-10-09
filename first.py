c580214c3a9bc257f2fdb8cff86d0585a3378399
metealpkurtoglu <146768140+metealpkurtoglu@users.noreply.github.com>
2023-10-09 21:32:55 +0300

    Update main.py

--- first.py
+++ first.py
@@ -1,2 +1,10 @@
-print("hi)
+import random 
+karakterler = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sifre_uzunlugu = int(input("Şifreniz Kaç karakter uzunluğunda olsun: "))
+
+sifre = ""
+for i in range(sifre_uzunlugu):
+    sifre += random.choice(karakterler)
+
+print(sifre)
       
